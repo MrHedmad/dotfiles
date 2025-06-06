7c65b2bf33bae807702216a0745e0acf5bd8c993
Luca Visentin <luca.visentin@unito.it>
2025-06-06 16:25:01 +0200

    Start using 'harpoon' in neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -359,6 +359,44 @@ require('lazy').setup({
     end,
   },
 
+  -- Harpoon, jump between files
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+
+      vim.keymap.set('n', '<leader>hf', function()
+        harpoon:list():add()
+      end, { desc = '[H]arpoon this [F]ile' })
+
+      -- Use Telescope windows with Harpoon
+      local conf = require('telescope.config').values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = conf.file_previewer {},
+            sorter = conf.generic_sorter {},
+          })
+          :find()
+      end
+
+      vim.keymap.set('n', '<leader>hh', function()
+        toggle_telescope(harpoon:list())
+      end, { desc = '[H]ello, [H]arpoon!' })
+    end,
+  },
+
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
