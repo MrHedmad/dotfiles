185875702d83ff52a6ea4ebc45decb3c9db27956
Luca Visentin <luca.visentin@unito.it>
2025-07-14 09:46:10 +0200

    feat: migrate which-key to new config

--- nvim/init.lua
+++ nvim/init.lua
@@ -228,21 +228,15 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup {
-        -- Blacklist the u key so we don't get an annoying reminder that uu is esc.
-        triggers_blacklist = {
-          i = { 'j', 'k' },
-          v = { 'j', 'k' },
-        },
-      }
+      require('which-key').setup {}
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '', group = '[C]ode' },
+        { '', group = '[D]ocument' },
+        { '', group = '[W]orkspace' },
+        { '', group = '[R]ename' },
+        { '', group = '[S]earch' },
       }
     end,
   },
