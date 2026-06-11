89c145006027990903852fcec89ed05464614170
Luca Visentin <luca.visentin@unito.it>
2026-06-11 10:24:02 +0200

    backup before resetting NVIM config

--- nvim/init.lua
+++ nvim/init.lua
@@ -737,12 +737,13 @@ require('lazy').setup({
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = "main",
     build = ':TSUpdate',
     config = function()
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
       ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup {
+      require('nvim-treesitter').setup {
         ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'rust', 'python', 'r' },
         -- Autoinstall languages that are not installed
         auto_install = true,
